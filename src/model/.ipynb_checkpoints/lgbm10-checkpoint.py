95bd5169b5331232e2cf39a119b3c2896f7aa081
hirokishinoda <progac27jhisi@gmail.com>
2021-04-07 17:04:30 +0900

    [new]特徴量選択を行い，重要度の低い特徴量を削除．

---
+++ src/model/.ipynb_checkpoints/lgbm10-checkpoint.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import lightgbm as lgb
+
+train_df = pd.read_pickle("../feature/select_train_df.pkl")
+
+Y_train = train_df["target"]
+X_train = train_df.drop("target",axis=1)
+
+print(Y_train.head())
+print(X_train.head())
+
+train_x, val_x, train_y, val_y = train_test_split(X_train, Y_train, test_size=0.3)
+
+train_data = lgb.Dataset(train_x, label=train_y)
+val_data = lgb.Dataset(val_x, label=val_y, reference=train_data)
+
+params = {
+    'boosting_type':'gbdt',
+    'objective':'binary',
+    'learning_rate': 0.05,
+    'max_depth': 5,
+    'min_data_in_leaf': 50,
+    'n_estimators': 200, 
+    'num_leaves': 15,
+    'seed':0
+}
+
+gbm = lgb.train(
+    params,
+    train_data,
+    valid_sets=val_data,
+    num_boost_round=1000,
+    verbose_eval=100
+)
+
+gbm.save_model('lgbm10.txt')
+
+y_pred = gbm.predict(val_x, num_iteration=gbm.best_iteration)
+
+print(y_pred)
+
+print("accuracy : ", accuracy_score(val_y.values.tolist(), y_pred.round(0).tolist()))
